10b7469f5b2220813ac582c9a3566e88c3ae365b
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2015-09-14 16:12:31 +0200

    Removed obsolete packages (agni_utils replaced by tactile_filgers, agni_tactile_viz replaced by tactile_marker_publisher, agni_grasp_force_limiter incorporated in HandServer directly) Only kept tactile_test script and moved to agni_description

--- agni_tactile_viz/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    scripts=['nodes/tactile_test_sensors.py'],
-    package_dir={'': 'nodes'})
-
-setup(**setup_args)
